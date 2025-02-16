1bc993b170638ffd8b155e24c54c799e4c894514
Kevin Montambault <kevin.t.montambault@gmail.com>
2025-02-16 17:44:17 -0500

    new build dir

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ const defineConfig = require('vite').defineConfig;
 export default defineConfig({
     root: 'frontend', // Set the root directory to /www/
     publicDir: 'static',
+    build: {
+        outDir: 'docs',
+    },
     server: {
         port: 5173, // Change if needed
     }
