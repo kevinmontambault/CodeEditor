2856a2344ab13fe0c153de29eae2712369d6d30d
Kevin Montambault <kevin.t.montambault@gmail.com>
2025-02-16 17:50:03 -0500

    Endpoint Update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ const defineConfig = require('vite').defineConfig;
 export default defineConfig({
     root: 'frontend', // Set the root directory to /www/
     publicDir: 'static',
+    base: '/CodeEditor/',
     build: {
         outDir: '../docs',
     },
